3354de64d2ea3f521e7895d5c8f4dad66032e7da
Joaquín Arregui Díaz <joaquin.arregui2002@gmail.com>
2022-11-10 12:23:16 +0100

    Player

--- src/main/java/org/springframework/samples/petclinic/player/Personage.java
+++ src/main/java/org/springframework/samples/petclinic/player/Personage.java
@@ -1,18 +1,9 @@
 package org.springframework.samples.petclinic.player;
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 
 import org.springframework.core.style.ToStringCreator;
-import org.springframework.samples.petclinic.model.Person;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/org/springframework/samples/petclinic/player/Rol.java
+++ src/main/java/org/springframework/samples/petclinic/player/Rol.java
@@ -1,21 +1,10 @@
 package org.springframework.samples.petclinic.player;
 
 
-import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 
 import org.springframework.core.style.ToStringCreator;
-import org.springframework.samples.petclinic.model.Person;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 import lombok.Getter;
 import lombok.Setter;
