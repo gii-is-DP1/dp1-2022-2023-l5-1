c7d7c809ca2db216ab5a05f6b079d91e475a7769
Joaquín Arregui Díaz <joaquin.arregui2002@gmail.com>
2022-11-10 11:25:09 +0100

    player

--- src/main/java/org/springframework/samples/petclinic/player/Player.java
+++ src/main/java/org/springframework/samples/petclinic/player/Player.java
@@ -15,28 +15,24 @@
  */
 package org.springframework.samples.petclinic.player;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
-import org.springframework.beans.support.MutableSortDefinition;
-import org.springframework.beans.support.PropertyComparator;
 import org.springframework.core.style.ToStringCreator;
 import org.springframework.samples.petclinic.model.Person;
-import org.springframework.samples.petclinic.pet.Pet;
 import org.springframework.samples.petclinic.user.User;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Simple JavaBean domain object representing an player.
@@ -46,129 +42,37 @@ import org.springframework.samples.petclinic.user.User;
  * @author Sam Brannen
  * @author Michael Isvy
  */
+@Getter
+@Setter
 @Entity
 @Table(name = "players")
 public class Player extends Person {
 
-	@Column(name = "address")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+	@Column(name = "nickname")
 	@NotEmpty
-	private String address;
+	private String Nickname;
 
-	@Column(name = "city")
+	@Column(name = "resistence")
+	@Min(value=0)
 	@NotEmpty
-	private String city;
+	private Integer resistence;
 
-	@Column(name = "telephone")
+	@Column(name = "honor")
 	@NotEmpty
-	@Digits(fraction = 0, integer = 10)
-	private String telephone;
+	@Min(value=0)
+	private String honor;
 
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "player")
-	private Set<Pet> pets;
+	@OneToOne()
+	private Rol rol;
 	
-	//
-	@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "username", referencedColumnName = "username")
-	private User user;
-	//
+	@OneToOne()
+    @JoinColumn(name = "character", referencedColumnName = "name")
+	private Character Character;
 	
-	public String getAddress() {
-		return this.address;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return this.city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getTelephone() {
-		return this.telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	protected Set<Pet> getPetsInternal() {
-		if (this.pets == null) {
-			this.pets = new HashSet<>();
-		}
-		return this.pets;
-	}
-
-	protected void setPetsInternal(Set<Pet> pets) {
-		this.pets = pets;
-	}
-
-	public List<Pet> getPets() {
-		List<Pet> sortedPets = new ArrayList<>(getPetsInternal());
-		PropertyComparator.sort(sortedPets, new MutableSortDefinition("name", true, true));
-		return Collections.unmodifiableList(sortedPets);
-	}
-
-	public void addPet(Pet pet) {
-		getPetsInternal().add(pet);
-		pet.setPlayer(this);
-	}
-	
-	public boolean removePet(Pet pet) {
-		return getPetsInternal().remove(pet);
-	}
-
-	/**
-	 * Return the Pet with the given name, or null if none found for this Player.
-	 * @param name to test
-	 * @return true if pet name is already in use
-	 */
-	public Pet getPet(String name) {
-		return getPet(name, false);
-	}
-	
-	public Pet getPetwithIdDifferent(String name,Integer id) {
-		name = name.toLowerCase();
-		for (Pet pet : getPetsInternal()) {
-			String compName = pet.getName();
-			compName = compName.toLowerCase();
-			if (compName.equals(name) && pet.getId()!=id) {
-				return pet;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Return the Pet with the given name, or null if none found for this Player.
-	 * @param name to test
-	 * @return true if pet name is already in use
-	 */
-	public Pet getPet(String name, boolean ignoreNew) {
-		name = name.toLowerCase();
-		for (Pet pet : getPetsInternal()) {
-			if (!ignoreNew || !pet.isNew()) {
-				String compName = pet.getName();
-				compName = compName.toLowerCase();
-				if (compName.equals(name)) {
-					return pet;
-				}
-			}
-		}
-		return null;
-	}
 
 	@Override
 	public String toString() {
