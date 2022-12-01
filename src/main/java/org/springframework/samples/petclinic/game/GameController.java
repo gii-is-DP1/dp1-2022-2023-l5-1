c8f784fce77ee8bdf52e3103b0c1816266df92e0
guigalde <guillermo.galeanodepaz@gmail.com>
2022-12-01 22:38:17 +0100

    Vista partida empezada,clase deck duplicada coment

--- src/main/java/org/springframework/samples/petclinic/game/GameController.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameController.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.game;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.samples.petclinic.deck.Deck;
 import org.springframework.samples.petclinic.player.Player;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,7 +11,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -20,6 +20,9 @@ public class GameController {
     private static final String ERROR = "/error";
     private static final String MESSAGE= "message";
     private static final String GAME_NOT_FOUND = "Game not found";
+    private static final String GAME="game";
+    private static final String PLAYERS="players";
+    private static final String DECK="deck";
 
     private GameService gameService;
 
@@ -37,16 +40,21 @@ public class GameController {
 
     @GetMapping(path="/{id}")
     public String viewGame(@PathVariable("id") Integer gameId, ModelMap modelMap){
-        String view = "game/playing";
+        String view = "games/ongoingGame";
         Optional<Game> optGame = gameService.findGameById(gameId);
         if(optGame.isPresent()){
             Game game = optGame.get();
+            modelMap.put(GAME, game);
+            Deck deck=game.getDeck();
+            modelMap.put(DECK, deck);
             List<Player> players = game.getPlayers();
+            modelMap.put(PLAYERS, players);
+            
         }else{
             modelMap.addAttribute(MESSAGE, GAME_NOT_FOUND);
             view= ERROR;
         }
-        return null;
+        return view;
     }
     
 
